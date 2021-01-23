c3119a065f6a4779ecc5aba6a0d73f436ca32278
vorzlemir <vorzelmir@gmail.com>
2021-01-24 01:06:44 +0200

    add crud to user model

--- app.js
+++ app.js
@@ -1,22 +1,37 @@
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 const userController = require('./controllers/v1/userController');
 const petController = require('./controllers/v1/petController');
 const nurseryController = require('./controllers/v1/nurseryController');
 
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+
 mongoose
-  .connect('mongodb://localhost/nursery', {
+  .connect(DB, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() => console.log('Connected to db...'))
+  .then((con) => {
+    console.log('Connected to db...');
+    console.log(con.connections);
+  })
   .catch((err) => console.error('Can not connect', err));
 
-petController.createPet('Rex', 'dog', 5, '600b3bf070e95249f86f5fad');
-userController.createUser(
-  'Bill',
-  'user@mail.com',
-  true,
-  '600b35a6a98172469f68b957',
-);
-nurseryController.createNursery('My Lovely Pets');
-// userController.listUsers();
+// petController.createPet('Rex', 'dog', 5, '600b3bf070e95249f86f5fad');
+// userController.createUser(
+//     'Bill',
+//     'user@mail.com',
+//     true,
+//     '600b35a6a98172469f68b957'
+// );
+// nurseryController.createNursery('My Lovely Pets');
+// userController.getAllUser();
+// userController.getUserByName('Bill');
+// userController.updateUserOne('600c6ba318d24f5226a2361c', 'Joe', 'joe@another-email.com', false, '600c6ba318d24f5226a2361b');
+// userController.updateUserTwo('600c6ba318d24f5226a2361c', 'Joe-Older', 'joe-older@another-email.com', true, '600c6ba318d24f5226a2361b');
+userController.deleteUser('600c4849c03dfa836f524541');
--- controllers/v1/userController.js
+++ controllers/v1/userController.js
@@ -14,10 +14,66 @@ exports.createUser = async (firstname, email, hasPet, pet) => {
     console.error('Error message: ', err);
   }
 };
-exports.listUsers = async () => {
-  const users = await User
-    .find()
-    .populate('pet')
-    .select('name');
-  console.log(users);
+
+exports.getAllUser = async () => {
+  try {
+    const users = await User
+      .find();
+    console.log(users);
+  } catch (err) {
+    console.error(err.message);
+  }
+};
+
+exports.getUserByName = async (name) => {
+  try {
+    const user = await User
+      .find({ firstname: name });
+    console.log(user);
+  } catch (err) {
+    console.error(err.message);
+  }
+};
+
+exports.updateUserOne = async (id, firstname, email, hasPet, pet) => {
+  try {
+    const user = await User.findById(id);
+    if (!user) {
+      return;
+    }
+    user.firstname = firstname;
+    user.email = email;
+    user.hasPet = hasPet;
+    user.pet = pet;
+
+    const result = await user.save();
+    console.log(result);
+  } catch (err) {
+    console.error(err.message);
+  }
+};
+
+exports.updateUserTwo = async (id, firstname, email, hasPet, pet) => {
+  try {
+    const user = await User.update({ _id: id }, {
+      $set: {
+        firstname,
+        email,
+        hasPet,
+        pet,
+      },
+    });
+    console.log(user);
+  } catch (err) {
+    console.error(err.message);
+  }
+};
+
+exports.deleteUser = async (id) => {
+  try {
+    const result = await User.deleteOne({ _id: id });
+    console.log(result);
+  } catch (err) {
+    console.error(err.message);
+  }
 };
